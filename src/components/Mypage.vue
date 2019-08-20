782d4362ad3c26106690bcf13b4476c311000f78
y-otsuka-dcm <sakunodcm@gmail.com>
2019-08-20 09:31:01 +0900

    2019/08/20

--- src/components/Mypage.vue
+++ src/components/Mypage.vue
@@ -1,7 +1,18 @@
 <template>
     <div class=top>
-    <h1>{{ msg }}</h1>
-    <h2>これからいろいろ書くよー！</h2>
+    <h1><img src="../assets/rmel.jpg" class="frame">{{ msg }}</h1>
+    <h2>自己紹介</h2>
+      <p>大きな事故とかはしたことないです</p>
+    <h2>経歴</h2>
+      <p></p>
+      <p>2017年4月 ドワンゴでエンジニアのアルバイトを始める</p>
+      <p>2019年3月 大学卒業に伴いドワンゴを退社</p>
+      <p>2019年4月 NTTグループR＆D部門のインフラエンジニアとして勤務</p>
+    <h2>スキル</h2>
+      <p>・JavaScript(React,Vue)</p>
+      <p>・Unity</p>
+      <p>・Python</p>
+      <p>・AWS、GCP</p>
     </div>
 </template>
 
@@ -10,7 +21,7 @@ export default {
   name: 'Mypage',
   data () {
     return {
-      msg: '~RMEL Portfolio~'
+      msg: 'RMEL Profile'
     }
   }
 }
@@ -18,6 +29,12 @@ export default {
 
 <style scoped>
 .top {
-    background-color:teal
+    background-color:teal;
+}
+
+.frame {
+  border-radius: 50%;
+  width: 80px;
+  height: auto;
 }
 </style>
